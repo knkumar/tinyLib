ed57c9c93c187a37dbb034a091422455f99d1d62
Kiran Kumar <krankumar@gmail.com>
2018-05-16 10:18:30 -0400

    Update qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
+// swap elements in array with index i and j
 void swap(int* arr, int i , int j){
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 
+// partition arrray using first element as the pivot
 int partition(int* arr, int start, int stop){
     int pivot = arr[start];
     int seen = start;
@@ -23,10 +25,15 @@ int partition(int* arr, int start, int stop){
     return seen;
 }
 
+// quick sort for an array of size stop-start
+// potentially can also sort only sub parts of the array
 void qsort(int* arr,int start,int stop){
     if (start < stop) {
+	// find pivot to start sorting
 	int pivot = partition(arr, start, stop);
+	// sort left of pivot
 	qsort(arr, start, pivot-1);
+	// sort right of pivot
 	qsort(arr, pivot+1, stop);
     }
 }
